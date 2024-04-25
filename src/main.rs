7f0d8947f95def59db97b2868fbca505d7afccec
amy <fabianpaci@protonmail.com>
2024-04-25 16:14:08 +1000

    whole number offset

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,9 @@ fn main() {
 }
 
 struct Model {
-    grid: Perlin,
-    cells: Vec<Cell>
+    grid: Vec<Vec2>,
+    cells: Vec<Cell>,
+    noise: Perlin
 }
 
 fn model(app: &App) -> Model {
@@ -22,29 +23,45 @@ fn model(app: &App) -> Model {
         .view(view)
         .build()
         .unwrap();
+    let window = app.window_rect();
 
-
+    let mut noise = Perlin::new().set_seed(random());
+    let mut grid: Vec<Vec2> = Vec::with_capacity(window.w() as usize *window.h() as usize);
+    for y in 0..window.h() as usize {
+        for x in 0..window.w() as usize {
+            let theta = ((noise.get([x as  f64 + 0.003, y as f64 + 0.01])+1.)*1./2.)*2.*PI as f64;
+            grid.push(Vec2::new(theta.cos() as _, theta.sin() as _));
+        }
+    }
 
     let mut cells = Vec::with_capacity(100);
     for _ in 0..cells.capacity() {
-        cells.push(Cell::new(random_in_window!(app.window_rect())))
+        cells.push(Cell::new(random_in_window!(window)))
     }
+
     Model {
-        grid: Perlin::new().set_seed(1),
-        cells
+        grid,
+        cells,
+        noise
     }
 }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    for cell in model.cells.iter_mut() {
-        let theta = (model.grid.get([cell.pos.x as _, cell.pos.y as _])+1.)*1./2.*2.*PI as f64;
-        cell.dir = Vec2::new(theta.cos() as _, theta.sin() as _);
-        cell.update()
+fn update(app: &App, model: &mut Model, _update: Update) {
+    for (i, cell) in model.cells.iter_mut().enumerate() {
+        // cell.dir = model.grid[(cell.pos.x + cell.pos.y * app.window_rect().w()) as usize];
+
+        let t = app.elapsed_frames() as f64/ 100.;
+        let x = model.noise.get([ cell.pos.x as f64 / 128., cell.pos.y as f64 / 137., t + i as f64/ 1000. ]);
+        let y = model.noise.get([ -cell.pos.y as f64 / 128., cell.pos.x as f64 / 137., t + i as f64/ 1000. ]);
+
+        cell.dir = vec2(x as f32, y as f32);
+
+
+        cell.update();
     }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
-    let t = app.time;
     let draw = app.draw();
 
     draw.background().color(BLACK);
@@ -57,7 +74,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
         draw.line()
             .color(GREEN)
-            .weight(5.)
+            .weight(1.)
             .start(cell.pos)
             .end(cell.pos + cell.dir.normalize()*50.);
     }
@@ -75,7 +92,7 @@ impl Cell {
     fn new(pos: Vec2) -> Self {
         Self {
             pos,
-            dir: Vec2::default()
+            dir: Vec2::default() + 100.
         }
     }
 
