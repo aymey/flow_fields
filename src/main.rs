261f60a852ba9897e06ac3b65556076d0b4ba144
amy <fabianpaci@protonmail.com>
2024-04-25 14:47:00 +1000

    static using perlin noise

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use nannou::prelude::*;
+use nannou::{noise::{NoiseFn, Perlin, Seedable}, prelude::*};
+
+macro_rules! random_in_window {
+    ($rect: expr) => {
+        Vec2::new(random_range(-$rect.w()/2., $rect.w()/2.), random_range(-$rect.h()/2., $rect.h()/2.))
+    };
+}
 
 fn main() {
     nannou::app(model).update(update).run();
 }
 
 struct Model {
-    grid: Grid
+    grid: Perlin,
+    cells: Vec<Cell>
 }
 
 fn model(app: &App) -> Model {
@@ -16,13 +23,24 @@ fn model(app: &App) -> Model {
         .build()
         .unwrap();
 
+
+
+    let mut cells = Vec::with_capacity(100);
+    for _ in 0..cells.capacity() {
+        cells.push(Cell::new(random_in_window!(app.window_rect())))
+    }
     Model {
-        grid: Grid::new(5, 200, app.window_rect()),
+        grid: Perlin::new().set_seed(1),
+        cells
     }
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    model.grid.update()
+    for cell in model.cells.iter_mut() {
+        let theta = (model.grid.get([cell.pos.x as _, cell.pos.y as _])+1.)*1./2.*2.*PI as f64;
+        cell.dir = Vec2::new(theta.cos() as _, theta.sin() as _);
+        cell.update()
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -31,125 +49,37 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     draw.background().color(BLACK);
 
-    for &field in model.grid.fields.iter() {
+    for cell in model.cells.iter() {
         draw.ellipse()
-            .radius(2.*(PI*t).sin())
+            .radius(5.)
             .color(BURLYWOOD)
-            .xy(field);
-    }
-    for cell in model.grid.cells.iter() {
-        draw.ellipse()
-            .radius(1.)
-            .color(GREY)
             .xy(cell.pos);
 
-        // debug
         draw.line()
-            .weight(1.)
             .color(GREEN)
+            .weight(5.)
             .start(cell.pos)
-            .end(cell.pos + cell.direction.normalize()*50.);
+            .end(cell.pos + cell.dir.normalize()*50.);
     }
-
 
     // Write the result of our drawing to the window's frame.
     draw.to_frame(app, &frame).unwrap();
 }
 
-macro_rules! random_in_window {
-    ($rect: expr) => {
-        Vec2::new(random_range(-$rect.w()/2., $rect.w()/2.), random_range(-$rect.h()/2., $rect.h()/2.))
-    };
-}
-
-// TODO: maybe each field can have an attraction strength (even negative like some repulse cells)
-#[derive(Default)]
-struct Grid {
-    fields: Vec<Vec2>,
-    cells: Vec<Cell>,
-    original: Vec<Cell>
-}
-
-#[derive(Default, Clone, Copy)]
 struct Cell {
     pos: Vec2,
-    direction: Vec2,
-
-    lifetime: u32,
-    id: usize,
+    dir: Vec2
 }
 
 impl Cell {
-    fn new(window: Rect, id: usize) -> Self {
-        let r = random_in_window!(window);
+    fn new(pos: Vec2) -> Self {
         Self {
-            pos: r,
-            direction: r,
-
-            lifetime: random_range(100, 900),
-            id
+            pos,
+            dir: Vec2::default()
         }
-    }
-
-    fn update(&mut self) -> Option<()> {
-        self.lifetime -= 1;
-        if self.lifetime == 0 {
-            return None;
-        }
-
-        self.pos += self.direction.normalize();
-        Some(())
-    }
-
-    fn revive(&mut self, replacement: Self) {
-        *self = replacement;
-    }
-}
-
-impl Grid {
-    const ATTRACTION: f32 = 1000.;
-
-    fn new(fields: usize, cells: usize, window: Rect) -> Self {
-        let mut grid = Self::default();
-        for _ in 0..fields {
-            grid.fields.push(random_in_window!(window))
-        }
-        for id in 0..cells {
-            let cell = Cell::new(window, id);
-            grid.cells.push(cell);
-            grid.original.push(cell);
-        }
-
-        grid
     }
 
     fn update(&mut self) {
-        for cell in self.cells.iter_mut() {
-            if let None = cell.update() {
-                cell.revive(self.original[cell.id])
-            }
-        }
-
-        self.attract()
+        self.pos += self.dir.normalize()
     }
-
-    fn attract(&mut self) {
-        for cell in self.cells.iter_mut() {
-            for &field in self.fields.iter() {
-                let direction = (field - cell.pos).normalize();
-
-                // cheap hacky way instead of dot product solve for theta and use that to get resultant direction
-                // since these vectors are noramlized they form a circle of radius 1 so we can comput the midpoint between two point of the circumference and normalize that for a good approx
-                cell.direction = midpoint(cell.direction, direction);
-
-                // let distance = cell.pos.distance(field);
-                // let attraction = distance/Self::ATTRACTION;
-                // cell.pos += cell.direction * attraction;
-            }
-        }
-    }
-}
-
-fn midpoint(a: Vec2, b: Vec2) -> Vec2 {
-    Vec2::new((a.x+b.x)/2., (a.y+b.y)/2.)
 }
